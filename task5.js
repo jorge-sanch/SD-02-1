efbf91852bcaaed074bb57e842bfe666a3e2528a
Jorge Sanchez <sanchez.j.jolu@gmail.com>
2025-12-15 15:31:18 -0600

    Se agrego la actividad 5

--- task5.js
+++ task5.js
@@ -1,5 +1,33 @@
-// Refer to Task 5 in your Instructions to complete this task
+// ¿Puedes pedirle al usuario que ingrese la cantidad de líneas que se generarán, o que genere una línea específica?
+
+const prompt = require("prompt-sync")();
+const num = Number(prompt("Ingrese el numero de lineas que quires que sean generadas: "));
 
 for (let i = 0; i < 1; i++) {
     console.log("This is Task Five!");
   };
+
+
+  for (let i = 0; i < 1; i++) {
+    console.log("This is Task Four!");
+  };
+
+for(let i = 1; i <= num; i++){
+  if(i % 7 == 0 &&i % 5 == 0 && i % 3 == 0){
+    console.log("FizzBuzzWoof")
+    }else if(i % 5 == 0 && i % 7 == 0){
+      console.log("BuzzWoof")
+      }else if(i % 7 == 0 && i % 3 == 0){
+        console.log("FizzWoff")
+        } else if(i % 5 == 0 && i % 3 == 0){
+          console.log("FizzBuzz")
+          }else if(i% 7 == 0){
+            console.log("Woof")
+            } else if(i % 5 ==0){
+              console.log("Buzz");
+              }else if( i % 3 == 0){
+                console.log("Fizz");
+                }else{
+                  console.log(i);
+                  }
+}
--- task6.js
+++ task6.js
@@ -1,5 +1,40 @@
-// Refer to Task 6 in your Instructions to complete this task
-
+// ¿Puedes asignar los números generados de la secuencia a elementos en una lista o arreglo?
 for (let i = 0; i < 1; i++) {
     console.log("This is Task Six!");
   };
+
+
+  
+const prompt = require("prompt-sync")();
+const num = Number(prompt("Ingrese el numero de lineas que quires que sean generadas: "));
+
+for (let i = 0; i < 1; i++) {
+    console.log("This is Task Five!");
+  };
+
+
+  for (let i = 0; i < 1; i++) {
+    console.log("This is Task Four!");
+  };
+
+const listaNumeros = [];
+
+for(let i = 1; i <= num; i++){
+  if(i % 7 == 0 &&i % 5 == 0 && i % 3 == 0){
+    console.log("FizzBuzzWoof"
+    }else if(i % 5 == 0 && i % 7 == 0){
+      console.log("BuzzWoof")
+      }else if(i % 7 == 0 && i % 3 == 0){
+        console.log("FizzWoff")
+        } else if(i % 5 == 0 && i % 3 == 0){
+          console.log("FizzBuzz")
+          }else if(i% 7 == 0){
+            console.log("Woof")
+            } else if(i % 5 ==0){
+              console.log("Buzz");
+              }else if( i % 3 == 0){
+                console.log("Fizz");
+                }else{
+                  console.log(i);
+                  }
+}
